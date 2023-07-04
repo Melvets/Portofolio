019a1b5d3e2e7a30979c7c9f6604820d4d83562b
Camela Devs <camelaputri1@gmail.com>
2023-07-04 21:14:05 +0700

    Menambahkan media query responsive hamburger pada index

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => { 
    navbarNav.classList.toggle('active'); 
   };
+
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger');
+
+document.addEventListener('click', function (e) {
+   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+     navbarNav.classList.remove('active');
+   }
+});
